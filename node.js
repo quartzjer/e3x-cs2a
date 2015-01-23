a9c87d6f4d6a60f6c9a3ce5326b90bf5ca290704
Jeremie Miller <jeremie@jabber.org>
2015-01-22 18:44:54 -0700

    use forge to work around ursa oddity

--- node.js
+++ node.js
@@ -12,13 +12,11 @@ try {
   var ecc = require("ecc-jsbn")
 }
 
+var forge = require("node-forge");
 try {
   if(process.env.PURE == 'true') throw new Error("pure requested");
   var ursa = require("ursa");
 }catch(E){
-  var forge = require("node-forge");
-  // feed some local entropy into it
-  forge.random.collect(crypto.randomBytes(2048).toString('binary'));
 }
 
 // load node-specific crypto methods
@@ -36,8 +34,11 @@ if(ursa)
   }
 
   exports.loadkey = function(id, key, secret)
-  {  
-    var pk = ursa.coercePublicKey(der2pem(key,"PUBLIC"));
+  {
+    // TODO, figure out why ursa is rejecting valid pub keys, workaround is using forge
+    var pkf = forge.pki.publicKeyFromAsn1(forge.asn1.fromDer(key.toString("binary")));     var pem = forge.pki.publicKeyToPem(pkf);
+    var pk = ursa.coercePublicKey(pem);
+//    var pk = ursa.coercePublicKey(der2pem(key,"PUBLIC"));
     if(!pk) return true;
     if(pk.getModulus().length != 256) return true;
     id.encrypt = function(buf){
@@ -74,6 +75,6 @@ function der2pem(der,type)
   if(!der || !Buffer.isBuffer(der)) return false;
   var b64 = der.toString("base64");
   if(!b64) return false;
-  b64 = b64.match(/.{1,60}/g).join("\n");
+  b64 = b64.match(/.{1,64}/g).join("\n");
   return "-----BEGIN "+type+" KEY-----\n"+b64+"\n-----END "+type+" KEY-----\n";  
 }
